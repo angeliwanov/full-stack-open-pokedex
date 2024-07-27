2dcecc1dad1ae0a3e45f2658108f60f9df985ab9
Angel Ivanov <angel.iwanov@gmail.com>
2024-07-27 08:59:48 +0300

    deployment render

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.static('dist'));
 
+app.get('/version', (res, _res) => {
+  res.send('1');
+});
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
